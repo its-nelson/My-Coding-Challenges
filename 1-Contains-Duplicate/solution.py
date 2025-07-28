5121a488d1fd872f04c8ad5e0a57a0b06e35afa0
Nelson Kariuki <kariukinelsonit@gmail.com>
2025-07-28 09:05:05 +0300

    Update solution.py
    I removed the for loop and just did a direct operation by: set(num)
    I previously did not know that was possible. It is way more efficient.

--- 1-Contains-Duplicate/solution.py
+++ 1-Contains-Duplicate/solution.py
@@ -1,8 +1,6 @@
 def containsDuplicate(nums):
     """Checks if an array contains any duplicate elements"""
-    set_of_nums = set()
-    for num in nums:
-        set_of_nums.add(num)
+    set_of_nums = set(nums)
     
     if len(nums) != len(set_of_nums):
         return True
