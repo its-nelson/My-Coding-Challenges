f04686fd5da6ddf399adbab945ad427964771edf
its-nelson <kariukinelsonit@gmail.com>
2025-07-31 17:16:30 +0300

    Updated the code to contain output based on given examples.

--- 1-Contains-Duplicate/solution.py
+++ 1-Contains-Duplicate/solution.py
@@ -2,7 +2,13 @@ def containsDuplicate(nums):
     """Checks if an array contains any duplicate elements"""
     set_of_nums = set(nums)
     
-    if len(nums) != len(set_of_nums):
-        return True
-    else:
-        return False
+    return len(nums) != len(set_of_nums)
+
+# Example 1
+print(containsDuplicate([1,2,3,1]))
+
+# Example 2
+print(containsDuplicate([1,2,3,4]))
+
+# Example 3
+print(containsDuplicate([1,1,1,3,3,4,3,2,4,2]))
