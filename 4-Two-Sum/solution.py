eb0aed0e5b3bb1b6cd5e2f1f8cd1eeb880174723
its-nelson <kariukinelsonit@gmail.com>
2025-08-01 09:31:50 +0300

    Updated the code to contain output based on the given examples.

--- 4-Two-Sum/solution.py
+++ 4-Two-Sum/solution.py
@@ -6,3 +6,12 @@ def twoSum(nums, target):
         if num_needed in nums_hashmap:
             return [i, nums_hashmap[num_needed]]
         nums_hashmap[num] = i
+
+# Example 1
+print(twoSum([2,7,11,15], 9))
+
+# Example 2
+print(twoSum([3,2,4], 6))
+
+# Example 3
+print(twoSum([3,3], 6))
